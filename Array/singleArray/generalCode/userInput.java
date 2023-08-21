0218a703fe644a0d6ff7de9307ea301ce52a7a5e
Shreema Dey Purba <122149624+shreema2347@users.noreply.github.com>
2023-08-22 00:54:49 +0530

    dsa

--- Array/singleArray/generalCode/userInput.java
+++ Array/singleArray/generalCode/userInput.java
@@ -12,7 +12,8 @@ public class userInput {
             arr[i]=sc.nextInt();
          }
          System.out.println("Output");
-         for (int i = 1; i < arr.length; i++) {
+         for (int i = 1; i <
+          arr.length; i++) {
             System.out.println(i);
          }
     }
