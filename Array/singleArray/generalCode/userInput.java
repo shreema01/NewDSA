916ce9926624b993125691fd2639385133be6bbc
Shreema Dey Purba <122149624+shreema2347@users.noreply.github.com>
2023-08-22 00:43:53 +0530

    dsa

--- Array/singleArray/generalCode/userInput.java
+++ Array/singleArray/generalCode/userInput.java
@@ -7,12 +7,12 @@ public class userInput {
         Scanner sc=new Scanner(System.in);
         int[] arr=new int[10];
         System.out.println("Enter element:");
-         for (int i = 0; i < arr.length; i++) {
+         for (int i = 1; i < arr.length; i++) {
 
             arr[i]=sc.nextInt();
          }
          System.out.println("Output");
-         for (int i = 0; i < arr.length; i++) {
+         for (int i = 1; i < arr.length; i++) {
             System.out.println(i);
          }
     }
